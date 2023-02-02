8411cfa6242a739ac43a2b74905c1e32a17d338e
FloridaLangara <106621772+FloridaLangara@users.noreply.github.com>
2023-02-02 09:44:38 -0800

    fix: Add Navigation and Search and TVShows Screen

---
+++ src/screens/SearchScreen.jsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text, View} from "react-native";
+
+const SearchScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>Search </Text>
+        </SafeAreaView>
+    )
+}
+
+export default SearchScreen;
---
+++ src/screens/TVShowsScreen.js
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text, View} from "react-native";
+
+const TVShowsScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text>TV Shows</Text>
+        </SafeAreaView>
+    )
+}
+
+export default TVShowsScreen;
