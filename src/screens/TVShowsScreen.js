c54da1d8ac6266d98076befbd16f468bea7bf5d8
FloridaLangara <106621772+FloridaLangara@users.noreply.github.com>
2023-02-02 22:46:10 -0800

    fix: Add TVShows Screen

--- src/screens/TVShowsScreen.js
+++ src/screens/TVShowsScreen.js
@@ -1,9 +1,53 @@
-import {SafeAreaView, Text, View} from "react-native";
+import React, {useEffect, useState} from "react";
+import {FlatList, SafeAreaView} from "react-native";
+import {fetchTvShowsByCategory} from "../api/movie";
+import BottomSheet from "../components/BottomSheet";
+import BottomSheetButton from "../components/BottomSheetButton";
+import Movie from "../components/Movie";
 
 const TVShowsScreen = () => {
+    const [tvShows, setTvShows] = useState([]);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedCategory, setSelectedCategory] = useState("popular");
+
+    const options = ["airing_today", "on_the_air", "popular", "top_rated"];
+
+    const fetchTVShowData = async  () => {
+        const data = await fetchTvShowsByCategory(selectedCategory);
+        setTvShows(data);
+    }
+
+    useEffect(() => {
+        fetchTVShowData();
+    }, [selectedCategory]);
+
     return (
         <SafeAreaView>
-            <Text>TV Shows</Text>
+            <BottomSheetButton
+                modalVisible={modalVisible}
+                onBottomSheetButtonPress={() => setModalVisible(true)}
+                title={selectedCategory}
+            />
+            <BottomSheet
+                onRequestClose={() => setModalVisible(!modalVisible)}
+                visible={modalVisible}
+                onClose ={() => setModalVisible(false)}
+                onCategoryChangePress={(value) => {
+                    setSelectedCategory(value);
+                }}
+                options={options}
+                selected={selectedCategory}
+            />
+            <FlatList
+                data={tvShows}
+                renderItem={({item}) =>
+                    <Movie
+                        image={item.poster_path}
+                        movieTitle={item.name}
+                        popularity={item.popularity}
+                        releaseDate={item.first_air_date}
+                    />}
+            />
         </SafeAreaView>
     )
 }
